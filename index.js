daeb12a7807a86f57964bcb3495338b5076f58ad
Emon Dev180 <shuvroemon.cse@gmail.com>
2024-04-22 12:00:51 +0600

    javascript concept  commit

---
+++ index.js
@@ -0,0 +1,150 @@
+//variable
+// const  name = 'Emon dev'
+
+// name = 'shuvro Dev'
+
+// console.log(name)
+
+//let 
+
+// let  sum = 5;
+// sum += 10;
+// console.log(sum)
+
+//Codition
+
+// 6 oparetor 
+// <,>,===,==,not Equal
+
+// ||(or oparator ) and &&(and oparetor )
+
+// or oparetor example 
+
+// let num = 10;
+
+// if(num > 5 || num > 15){
+//     console.log('Condition true');
+// }
+// else{
+//     console.log('condotion false');
+// }
+
+/**and oparetor  */
+// let num = 10;
+
+//  if(num > 5 && num > 15){
+//     console.log('Condition true');
+//  }
+//  else{
+//    console.log('condotion false');
+//  }
+
+/**ARRAY */
+
+//let num = 1;
+//let num = 2;
+//let num = 3;
+// //let num = 4;
+
+// const num = [1,2,30];
+
+// console.log(num.length);//  length start 1  and index stat 0
+
+// // length is 3 and index is 2;
+
+// /**FOR LOOP */
+
+// const num = [10,20,30];
+
+
+// for(i=0;i<num.length;i++){
+
+//     console.log(num[i])
+// }
+/**FUNCTION */
+
+// function hello(num1,num2){
+//     let result = num1 * num2;
+
+//     console.log(result)
+
+  
+
+// }
+
+
+// // hello(5,5)
+
+// const hello = (num1 = 5,num2 = 10) => {
+
+//     const sum = num1 + num2;
+
+//     console.log(sum)
+
+    
+
+
+// }
+// hello()
+
+/**Object */
+
+
+const person = {
+    name:'Emon Dev',
+    age: 12,
+    phone:'01766688'
+}
+
+// // dot notaton 
+
+// console.log(person.name);
+
+// // thired bracce nptation 
+
+// console.log(person['age']);
+
+// //access dynamic 
+
+// const myVaiable = 'phone'
+
+// console.log(person[myVaiable])
+
+const {age,phone} = person;
+console.log(age,phone)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
